979acacc6ffe3b35b000ce24fdf8b30396e907bd
Shaban Daja <shabandaja710@gmail.com>
2023-10-05 15:45:26 +0200

    Can see the dark mode colors

--- script.js
+++ script.js
@@ -2,7 +2,11 @@ const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
 // Switch Theme Dynamicaly
 function switchTheme(event) {
-  console.log(event.target.checked);
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
 }
 
 // Event listener
